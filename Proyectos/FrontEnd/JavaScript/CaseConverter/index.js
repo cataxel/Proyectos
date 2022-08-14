0b5539ffd6039d19e10c7343396edf001baa1d47
Axel <axelllopez2002@gmail.com>
2022-08-13 19:05:44 -0500

    proyecto terminado

--- Proyectos/FrontEnd/JavaScript/CaseConverter/index.js
+++ Proyectos/FrontEnd/JavaScript/CaseConverter/index.js
@@ -19,3 +19,17 @@ let btnSe = document.getElementById("sentence-case").onclick = function () {
     }
     textarea.value = stre.join('. ');
 };
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
